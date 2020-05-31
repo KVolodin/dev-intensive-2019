7cc6469eebaab96bf00ad75b6c842362b5c3934d
kirill <lagons@mail.ru>
2020-05-31 18:15:45 +0300

    correction 2

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -14,7 +14,7 @@ abstract class BaseMessage (
     abstract fun formatMessage() : String
     companion object AbstractFactory{
         var lastId = -1
-        fun makeMessage(from: User?,chat: Chat,date: Date = Date(),type :String = "text",payload:Any?,isInComing: Boolean= false) : BaseMessage {
+        fun makeMessage(from: User?,chat: Chat,date: Date = Date(),payload:Any?,type :String = "text",isInComing: Boolean= false) : BaseMessage {
             lastId++
             return when(type) {
                 "image"->ImageMessage("$lastId",from,chat,isInComing,date = date,image = payload as String)
